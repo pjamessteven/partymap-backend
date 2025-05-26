bee11353568342a22234a3128fc1da7d4eb88df1
Your Name <pjamessteven@gmail.com>
2025-05-26 19:09:03 +1200

    improve getting lineup from text or image

--- pmapi/services/lineup.py
+++ pmapi/services/lineup.py
@@ -13,40 +13,18 @@ DEV_ENVIRON = get_debug_flag()
 CONFIG = DevConfig if DEV_ENVIRON else ProdConfig
 
 
-def get_lineup_from_text(text):
-    result = dify_request(CONFIG.DIFY_LINEUP_KEY, {'lineup_text': text })
-    if result:
-        result = json.loads(result)
-        result =  result.get('items', [])
-        return result
-    else: 
-        return []
-
-def get_lineup_from_image(image_url):
-    # can accept base64 or image URL
-    if (image_url):
-        result = dify_request(CONFIG.DIFY_LINEUP_KEY, {'lineup_image': [{'type': 'image', 'transfer_method': 'remote_url', 'url': image_url}]})
-
-        if result:
-            result = json.loads(result)
-            return result.get('items', [])
-        
-    return []
-    
 def get_lineup_from_image_and_text(event_id, lineup_text, lineup_image_url):
     event = get_event_or_404(event_id)
     next_ed = event.next_event()
     
-    lineup = get_lineup_from_text("event name: " + event.name + "/ndescription: " + lineup_text)
+    lineup = dify_request(CONFIG.DIFY_LINEUP_KEY, {'lineup_image': [{'type': 'image', 'transfer_method': 'remote_url', 'url': lineup_image_url}], 'lineup_text': lineup_text, 'event_start': event.start_naive })
 
-    if not lineup or lineup and len(lineup) == 0:
-        print('LINEUP getting from img')
-        lineup = get_lineup_from_image(lineup_image_url)
-
-    print('LINEUP result: ', lineup)
-
-    if lineup: 
+    if lineup:
+        lineup = json.loads(lineup)
+        lineup = lineup.get('lineup', [])
         add_artists_to_date(next_ed, lineup)
         db.session.commit()
+        print('LINEUP result: ', lineup)
+
     else:
         print('LINEUP not found for ' + event.name)
